1a6dc6fd99443a420d6c6633115599995301554e
Gururajan Srinivasan <Gururajan_S@ad.infosys.com>
2019-11-15 17:38:43 -0800

    update3

--- vscode_upload_to_s3.py
+++ vscode_upload_to_s3.py
@@ -1,4 +1,17 @@
 import boto3
 from botocore.client import Config
 
-ACCESS_KEY_ID
+ACCESS_KEY_ID =""
+ACCESS_SECRET_KEY=""
+BUCKET_NAME =""
+
+data = open('test.png', 'rb')
+
+s3 =boto3.resource(
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    Config=Config(signature_version='s3v4')
+)
+s3.Bucket(BUCKET_NAME).put_object(Keys='test.png', Body=data)
+
+print("Done")
